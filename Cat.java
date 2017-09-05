72bde37f02331463f2450d9ae63c4cfeaf7da099
bob <bob@APCSA>
2017-09-05 14:51:39 -0700

    Initial commit

--- Cat.java
+++ Cat.java
@@ -4,5 +4,19 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 public class Cat 
 {
-
+	private String breed;
+	private String name;
+	private boolean hasHair;
+	public Cat()
+	{
+		this.breed = "";
+		this.name = "";
+		this.hasHair = false;
+	}
+	public Cat(String breed, String name, boolean hasHair)
+	{
+		this.breed = breed;
+		this.name = name;
+		this.hasHair = hasHair;
+	}
 }
